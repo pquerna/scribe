d82d93b92123aecd41de9b4ccdf7a548570d791b
Paul Querna <pquerna@apache.org>
2010-04-26 17:05:37 -0700

    Improve the dummy stdout module

--- src/modules/stdout.cpp
+++ src/modules/stdout.cpp
@@ -23,7 +23,73 @@ class StdoutStore : public Store {
 public:
   StdoutStore(const std::string& category,
               bool readable, bool multi_category);
+  ~StdoutStore();
+
+  bool handleMessages(boost::shared_ptr<logentry_vector_t> messages);
+  bool open();
+  bool isOpen();
+  void configure(pStoreConf configuration);
+  void close();
+  void flush();
+  boost::shared_ptr<Store> copy(const std::string &category);
+
 };
+
+StdoutStore::StdoutStore(const std::string& category,
+                         bool readable, bool multi_category):
+  Store(category, "stdout", multi_category)
+{
+  
+}
+
+StdoutStore::~StdoutStore()
+{
+  flush();
+}
+
+bool StdoutStore::isOpen()
+{
+  return true;
+}
+
+bool StdoutStore::open()
+{
+  return true;
+}
+
+void StdoutStore::close()
+{
+  flush();
+}
+
+void StdoutStore::flush()
+{
+  fflush(stdout);
+}
+
+void StdoutStore::configure(pStoreConf conf)
+{
+}
+
+bool StdoutStore::handleMessages(boost::shared_ptr<logentry_vector_t> messages)
+{
+  for (logentry_vector_t::iterator iter = messages->begin();
+       iter != messages->end();
+       ++iter) {
+    logentry_ptr_t m = (*iter);
+    /* could buffer this into a single fwrite, but meh, this works for a demo */
+    fprintf(stdout, "[%s] ", m->category.c_str());
+    fwrite(m->message.data(), m->message.size(), 1, stdout);
+    fwrite("\n", 1, 1, stdout);
+  }
+  return true;
+}
+
+boost::shared_ptr<Store> StdoutStore::copy(const std::string &category) {
+  StdoutStore *store = new StdoutStore(category, false, multiCategory);
+  boost::shared_ptr<Store> copied = boost::shared_ptr<Store>(store);
+  return copied;
+}
 
 
 Store* stdout_store_create(void *baton, const std::string& category,
@@ -34,9 +100,10 @@ Store* stdout_store_create(void *baton, const std::string& category,
 
 void stdout_register(scribe_module_t *module, scribeHandler *scribe)
 {
-  Store::registerStore("stdout", stdout_store_create, NULL);
+  std::string n = "stdout";
+  Store::registerStore(n, stdout_store_create, NULL);
 }
 
 extern "C" {
-  SCRIBE_MODULE_DECL(stdout, stdout_register)
+  SCRIBE_MODULE_DECL(stdout, stdout_register);
 };
